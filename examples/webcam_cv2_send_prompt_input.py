7aa269277bb2b5f5830e59e90b849a9d75f817ba
beduffy <benfduffy@gmail.com>
2023-12-22 19:15:24 +0000

    Added opencv interface to camera. Press s key to ask for python input() and then send

---
+++ examples/webcam_cv2_send_prompt_input.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+import uuid
+
+import gradio as gr
+import numpy as np
+
+import webcamgpt
+
+connector = webcamgpt.OpanAIConnector()
+
+# export OPENAI_API_KEY=""
+
+# import the opencv library 
+import cv2 
+  
+vid = cv2.VideoCapture(0)
+  
+while(True): 
+    ret, frame = vid.read() 
+  
+    cv2.imshow('frame', frame) 
+      
+    k = cv2.waitKey(1)
+    if k == ord('q'): 
+        break
+
+    if k == ord('s'):
+        prompt = input('Write your prompt:\n')
+        print('Your prompt was: ', prompt)
+        response = connector.simple_prompt(image=frame, prompt=prompt)
+        print('Response:')
+        print(response)
+        # TODO save image and show elsewhere or? 
+  
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
